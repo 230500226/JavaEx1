15cda4753c7b928f2bfbb8598142062c1d72276d
230500226 <125464717+230500226@users.noreply.github.com>
2024-07-24 19:53:52 +0200

    initNetbeansMaven

---
+++ src/main/java/com/mycompany/javaex1/JavaEx1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javaex1;
+
+/**
+ *
+ * @author ldxt460s
+ */
+public class JavaEx1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
