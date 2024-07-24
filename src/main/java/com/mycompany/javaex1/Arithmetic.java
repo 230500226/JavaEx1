5f83a74badfd1640839b493343d0f137e4aa249e
230500226 <125464717+230500226@users.noreply.github.com>
2024-07-24 20:31:08 +0200

    Q1Arithmetic

--- src/main/java/com/mycompany/javaex1/Arithmetic.java
+++ src/main/java/com/mycompany/javaex1/Arithmetic.java
@@ -9,5 +9,25 @@ package com.mycompany.javaex1;
  * @author ldxt460s
  */
 public class Arithmetic {
+
+    // Add two integers
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // Subtract two integers
+    public int subtract(int a, int b) {
+        return a -b;
+    }
+
+    // Multiply two integers
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    // Divide two integers
+    public int divide(int a, int b) {
+        return a / b;
+    }
     
 }
--- src/main/java/com/mycompany/javaex1/JavaEx1.java
+++ src/main/java/com/mycompany/javaex1/JavaEx1.java
@@ -8,9 +8,41 @@ package com.mycompany.javaex1;
  *
  * @author ldxt460s
  */
+
+import java.util.Scanner;
 public class JavaEx1 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Enter you first integer");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        System.out.println("Enter the operation (+, -, *, /)");
+        String operation = scanner.next();
+        System.out.println("Enter you second integer");
+        int b = scanner.nextInt();
+
+        Arithmetic arithmetic = new Arithmetic();
+        double result = 0;
+        switch (operation) {
+            case "+":
+                result = arithmetic.add(a, b);
+                break;
+            case "-":
+                result = arithmetic.subtract(a, b);
+                break;
+            case "*":
+                result = arithmetic.multiply(a, b);
+                break;
+            case "/":
+                result = arithmetic.divide(a, b);
+                break;
+            default:
+                System.out.println("Invalid operation");
+        }
+
+        System.out.printf("Result of %d and %d is = %.2f\n", a, b, result);
+
+        scanner.close(); 
     }
 }
