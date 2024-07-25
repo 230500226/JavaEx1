0c0e0610aba56a10bea4c601d2bb954fb55e0aa0
230500226 <125464717+230500226@users.noreply.github.com>
2024-07-25 19:10:55 +0200

    added Statistics.java Q3

--- src/main/java/com/mycompany/javaex1/Statistics.java
+++ src/main/java/com/mycompany/javaex1/Statistics.java
@@ -8,6 +8,52 @@ package com.mycompany.javaex1;
  *
  * @author ldxt460s
  */
+import java.util.Scanner;
 public class Statistics {
+
+ // Ask and get three intergers from the user
+    public void start() {
+        System.out.println("Enter the first integer");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        System.out.println("Enter the second integer");
+        int b = scanner.nextInt();
+        System.out.println("Enter the third integer");
+        int c = scanner.nextInt();
+        scanner.close();
+
+        // Find the sum
+        int sum = a + b + c;
+        System.out.println("The sum is " + sum);
+
+        // Find the average
+        double average = (a + b + c) / 3.0;
+        System.out.println("The average is " + average);
+
+        // Find the product
+        int product = a * b * c;
+        System.out.println("The product is " + product);
+        
+        // Find the maximum
+        int max = a;
+        if (b > max) {
+            max = b;
+        }
+        if (c > max) {
+            max = c;
+        }
+        System.out.println("The maximum is " + max);
+        
+        // Find the minimum
+        int min = a;
+        if (b < min) {
+            min = b;
+        }
+        if (c < min) {
+            min = c;
+        }
+        System.out.println("The minimum is " + min);
+        
+    }
     
 }
